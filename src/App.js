56c1bf44ffa2b4a06d62b25c4625bb5e1b9855f6
Biljana Kotevska <biljanakotev@gmail.com>
2023-09-12 19:37:04 +0200

    added code from the student portal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import './App.css'
-import Navbar from './components/Navbar.jsx'
-import Home from './components/Home.jsx'
+import Navbar from './components/Navbar'; // <== IMPORT
+import HomePage from './components/HomePage'; // <== IMPORT
+import AboutPage from './components/AboutPage'; // <== IMPORT
+import ProjectsPage from './components/ProjectsPage'; // <== IMPORT
+import ErrorPage from './components/ErrorPage'; // <== IMPORT
+
+import { Routes, Route } from 'react-router-dom'; // <== IMPORT
 
 function App() {
-	return (
-		<div className='App'>
-			<Navbar />
-			<Home />
-		</div>
-	)
+  return (
+    <div className="App">
+      <Navbar />
+
+      <Routes>
+        {<Route path="/" element={<HomePage />} />}
+
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
